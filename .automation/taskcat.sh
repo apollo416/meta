090209686381b0e5499f1e3346cfc08b75c23a1b
Tarcísio Ferreira Sassara <sassara.tarcisio@gmail.com>
2024-04-30 02:38:37 -0300

    Update taskcat.sh

--- .automation/taskcat.sh
+++ .automation/taskcat.sh
@@ -1,17 +1,11 @@
 #/bin/bash
 
 python_path=$(which python3)
-temp_path=$(mktemp setup-taskat-XXX)
 
 $python_path --version
-venv_path="$temp_path/.venv"
-$python_path -m venv "$venv_path"
+$python_path -m venv .venv
 
-bin_path="$venv_path/bin"
-
-source "$bin_path/activate"
-python_path="$bin_path/python"
-
-$python_path -m pip install --upgrade pip
-$python_path -m pip install --upgrade setuptools wheel
-$python_path -m pip install --upgrade taskcat
+source .venv/bin/activate
+python -m pip install --upgrade pip
+python -m pip install --upgrade setuptools wheel
+python -m pip install --upgrade taskcat
